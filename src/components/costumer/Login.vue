d58d948430470d92ba1db23d39a7e78359e2c37c
LeoKucing <marifakbarvix@gmail.com>
2025-07-08 01:16:20 +0700

    10

--- src/components/costumer/Login.vue
+++ src/components/costumer/Login.vue
@@ -20,7 +20,7 @@ const password = ref('')
 const pesan = ref('')
 
 function login() {
-    fetch('https://leokucing.github.io/api/db.json')
+    fetch('https://api.example.com/data/db.json')
         .then(res => res.json())
         .then(users => {
         const user = users.find(u => u.username === username.value && u.password === password.value)
