a81b62a04268c25cd08d27a2b37ff3f83227eb1f
LeoKucing <marifakbarvix@gmail.com>
2025-07-07 17:53:43 +0700

    8

--- src/components/costumer/Login.vue
+++ src/components/costumer/Login.vue
@@ -20,7 +20,7 @@ const password = ref('')
 const pesan = ref('')
 
 function login() {
-    fetch('https://p10pbk-production.up.railway.app/users')
+    fetch('http://localhost:3000/users')
         .then(res => res.json())
         .then(users => {
         const user = users.find(u => u.username === username.value && u.password === password.value)
